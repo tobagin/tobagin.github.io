8f8309c5251afa35ae1d7a01606d2002b761b6e1
Thiago Fernandes <140509353+tobagin@users.noreply.github.com>
2025-07-22 15:28:44 +0100

    Update app_generator.rb

--- _plugins/app_generator.rb
+++ _plugins/app_generator.rb
@@ -1,13 +1,7 @@
 # _plugins/app_generator.rb
-puts "== plugin loaded =="
-
 Jekyll::Hooks.register :site, :post_read do |site|
-  puts "== hook fired, apps.yml contains #{site.data['apps'].size} entries =="
-
-  site.data['apps']&.each do |app|
+  (site.data['apps'] || []).each do |app|
     slug = app['slug']
-    puts "  generating /apps/#{slug}/index.html"
-
     page = Jekyll::PageWithoutAFile.new(
       site,
       site.source,
